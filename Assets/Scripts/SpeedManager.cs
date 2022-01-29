bf41a5a0e938238da4f79810af751a5cf7c54bb0
Ethan Akester <ethan.akester@mail.utoronto.ca>
2022-01-29 12:01:05 -0800

    added audio

--- Assets/Scripts/SpeedManager.cs
+++ Assets/Scripts/SpeedManager.cs
@@ -9,11 +9,17 @@ public class SpeedManager : MonoBehaviour
     public static float playerMovementScaling = 1f;
     public static float bulletSpeedScaling = 1f;
     public static float enemySpawnScaling = 1f;
+
+    static AudioSource music;
+    void Start() {
+        music = GameObject.Find("AudioManager").GetComponent<AudioSource>();
+    }
     public static void updateSpeeds(float healthRatio) {
 
         playerMovementScaling = healthRatio;
         bulletSpeedScaling = healthRatio;
         enemySpawnScaling = healthRatio;
+        music.pitch = healthRatio;
     }
 
 
